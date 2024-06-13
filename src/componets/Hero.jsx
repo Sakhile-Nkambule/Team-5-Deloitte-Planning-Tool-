f0037e5286fb2993b069fc7514f6dc8602ff5e96
Sakhile-Nkambule <nkmsqak006@myuct.ac.za>
2024-06-13 16:15:50 +0200

    Color change

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -3,7 +3,7 @@
 const Hero = ({title =' Plan Efficiently', subtitle ='Use the tool for effective resource allocations, project management and project planning',}) => {
   return (
 
-     <section className="bg-green-300 py-20 mb-4">
+     <section className="bg-lime-500 py-20 mb-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
           <div className="text-center">
             <h1 className="text-4xl font-extrabold text-black sm:text-5xl md:text-6xl">
