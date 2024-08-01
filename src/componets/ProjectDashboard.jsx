e5c7996a088273b18460386bf6292a288dd197dd
tadiwa mukudzavu <mkdtad001@myuct.ac.za>
2024-08-01 23:09:26 +0200

    TadiwaCommit

--- src/componets/ProjectDashboard.jsx
+++ src/componets/ProjectDashboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom';
+const linkClass = ({isActive})=> isActive? 
+    "text-black bg-lime-100 hover:bg-lime-500 hover:text-black rounded-md px-3 py-2" 
+    :"text-black hover:bg-lime-500 hover:text-black rounded-md px-3 py-2";
 const ProjectDashboard = () => {
   return (
-    <h2 className="text-green-500 text-3xl text-center font-semibold mb-25">Project Dashboard Currently Not Available</h2>
+    <div>
+      <h2 className="text-green-500 text-3xl text-center font-semibold mb-25">Project Dashboard Currently Not Available</h2>
+    <NavLink
+      to={`/taskboard/${1}`}
+      className={linkClass}
+    >
+      Task board
+    </NavLink>
+    </div>
   );
 };
 
---
+++ src/pages/TaskBoard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import KanbanBoard from '../componets/KanbanBoard'
+import Task from '../componets/Task'
+function TaskBoard() {
+  return (
+    <KanbanBoard/>
+  )
+}
+
+export default TaskBoard
