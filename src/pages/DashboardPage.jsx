fcb9f74b8021e8efc79dd4cd0f90c6735d7e890d
tadiwa mukudzavu <mkdtad001@myuct.ac.za>
2024-09-23 21:43:07 +0200

    NewDashBoardLayout yet to add functionality

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -6,6 +6,8 @@ import { useLoaderData } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useUser } from '../componets/UserContext'; // Assuming you're using a UserContext to manage user state
 import Calendar from '../componets/Calendar';
+import DashboardHearder from '../componets/DashboardHearder';
+
 const DashboardPage = () => {
   const { project, client, resources } = useLoaderData();
   const { user } = useUser(); // Get the logged-in user
@@ -13,12 +15,20 @@ const DashboardPage = () => {
 
 
   return (
-    <>
+    <div>
        {/* <Calendar/> */}
-      <ProjectDashboard />
-      <AvailableResources />
+      <DashboardHearder />
+      {/* <ProjectDashboard /> */}
+      <div className="flex bg-gray-100 shadow-xl p-4  grid grid-cols-3 gap-4">
+      
+      <div class="col-span-2 bg-white rounded-xl p-4">
+       <AvailableResources /> 
+      </div>
+      <div class="col-span-1 bg-white rounded-xl p-4">
       <BudgetUser project={project} />
-    </>
+      </div>
+      </div>
+    </div>
   );
 };
 
