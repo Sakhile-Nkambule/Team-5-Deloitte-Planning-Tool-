1d1275151504a577333a13f371c84d4bf339673b
tadiwa mukudzavu <mkdtad001@myuct.ac.za>
2024-10-26 11:33:03 +0200

    Have made changes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api':{
-        target: 'http://3.80.71.14:8081',
+        target: 'http://localhost:8081',
         changeOrigin: true,
         rewrite: (path)=>path.replace(/^\/api/,''),
         
